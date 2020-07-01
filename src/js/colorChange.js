447cdab3a2c599f9a8fd63a8f52e2011180e7437
cesar puentes <caps2695@gmail.com>
2020-07-01 18:37:08 -0500

    optimizing code at colorChange.js

--- src/js/colorChange.js
+++ src/js/colorChange.js
@@ -1,47 +1,21 @@
-const buttonHome = document.getElementById("btn-home");
-const buttonAbout = document.getElementById("btn-about");
-const buttonBriefcase = document.getElementById("btn-briefcase");
-const buttonBlog = document.getElementById("btn-blog");
-const buttonContact = document.getElementById("btn-contact");
-
-let randomValue = () => Math.floor(Math.random() * 256);
-let stringRgb = () =>
+export let randomValue = () => Math.floor(Math.random() * 256);
+export let stringRgb = () =>
   `rgb(${randomValue()}, ${randomValue()}, ${randomValue()})`;
 
-let countHome = 0;
-let countAbout = 0;
-let countBriefcase = 0;
-let countBlog = 0;
-let countContact = 0;
+let count = 0;
 
-function colorHome(item) {
-  buttonHome.style.backgroundColor = stringRgb();
-  countHome++;
-  console.log(`id: ${item.id}   count: ${countHome}`);
+const btnList = document.querySelectorAll("a");
+
+btnList.forEach((a) => {
+  a.addEventListener("click", randomColor);
+});
+
+function randomColor(event) {
+  count++;
+  console.log("clicked", event.target.innerHTML, "count", count);
+
+  btnList.forEach((a) => {
+    a.style.backgroundColor = "#cccccc";
+  });
+  event.target.style.backgroundColor = stringRgb();
 }
-
-function colorAbout(item) {
-  buttonAbout.style.backgroundColor = stringRgb();
-  countAbout++;
-  console.log(`id: ${item.id}   count: ${countAbout}`);
-}
-
-function colorBriefcase(item) {
-  buttonBriefcase.style.backgroundColor = stringRgb();
-  countBriefcase++;
-  console.log(`id: ${item.id}   count: ${countBriefcase}`);
-}
-
-function colorBlog(item) {
-  buttonBlog.style.backgroundColor = stringRgb();
-  countBlog++;
-  console.log(`id: ${item.id}   count: ${countBlog}`);
-}
-
-function colorContact(item) {
-  buttonContact.style.backgroundColor = stringRgb();
-  countContact++;
-  console.log(`id: ${item.id}   count: ${countContact}`);
-}
-
-module.exports = randomValue;
